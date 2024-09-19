a0fdab57df5f73c3a9bb5d164e73919a98d3324b
Ravi Thakur <phenominal9159@gmail.com>
2024-09-19 21:04:04 +0530

    made take challenge button interactive

--- frontend/components/Challenge/TakeChallengeButton.tsx
+++ frontend/components/Challenge/TakeChallengeButton.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { signIn, useSession } from "next-auth/react";
+import { useSession } from "next-auth/react";
 import { Button } from "../ui/button";
 import { useRouter } from "next/navigation";
 import axiosInstance from "@/lib/axios";
@@ -8,6 +8,7 @@ import { useState } from "react";
 import { QueryClient } from "@tanstack/react-query";
 import ChallengeStatusButton from "./ChallengeStatusButton";
 import { TakenChallengeStatus } from "@/types/challenges";
+import { useToast } from "@/hooks/use-toast";
 
 const TakeChallengeButton = ({
   text,
@@ -19,28 +20,46 @@ const TakeChallengeButton = ({
   challengeSlug?: string;
 }) => {
   const [challengeTaken, setChallengeTaken] = useState<boolean>(false);
+  const [isPending, setIsPending] = useState<boolean>(false);
   const { data: session, status } = useSession();
   const router = useRouter();
   const queryClient = new QueryClient();
+
+  const { toast } = useToast();
 
   const handleClick = async () => {
     if (status === "unauthenticated")
       router.push("/signin?callbackUrl=" + window.location.pathname);
 
     if (status === "authenticated") {
+      setIsPending(true);
       try {
         const res = await axiosInstance.post("/challenge/take-new", {
           challenge_id: challengeId,
         });
-        console.log("New Challenge:", res.data);
+        // console.log("New Challenge:", res.data);
+
         setChallengeTaken(true);
+
+        toast({
+          title: "Success",
+          description: "Challenge taken successfully",
+          variant: "success",
+        });
+
         // refetch the data
         queryClient.invalidateQueries({
           queryKey: ["challenge", challengeSlug],
         });
-      } catch (error) {
-        console.log(error);
+      } catch (error: any) {
+        // console.log(error);
+        toast({
+          title: "Error",
+          description: error.response.data.detail,
+          variant: "destructive",
+        });
       }
+      setIsPending(false);
     }
   };
   return (
@@ -53,7 +72,7 @@ const TakeChallengeButton = ({
           challengeTitle=""
         />
       ) : (
-        <Button size="lg" onClick={handleClick}>
+        <Button size="lg" onClick={handleClick} disabled={isPending}>
           {text}
         </Button>
       )}
